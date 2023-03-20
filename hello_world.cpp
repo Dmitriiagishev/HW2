224dece3a2a3c73e2f1a04c3b0605f6bd715f43c
Dmitriiagishev <triumfal0801@yandex.ru>
2023-03-20 09:00:16 +0300

    second commit with good codestyle and comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,8 +2,8 @@
 
 int main(){
 	std::string username;
-	std::cout << "enter username\n";
-	std::cin >> username;
-	std::cout << "Hello World from " << username;
+	std::cout << "enter username\n"; 
+	std::cin >> username; // entering username
+	std::cout << "Hello World from " << username; // main purpose
 	return 0;
 }
