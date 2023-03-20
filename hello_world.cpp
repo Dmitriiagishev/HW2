df80292fdc15f6670e63ef3f793174334a56cf14
Dmitriiagishev <triumfal0801@yandex.ru>
2023-03-20 08:54:12 +0300

    first commit with good codestyle

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 
-using namespace std;
-
 int main(){
-	string username;
-	cout << "enter username\n";
-	cin >> username;
-	cout << "Hello World from " << username;
+	std::string username;
+	std::cout << "enter username\n";
+	std::cin >> username;
+	std::cout << "Hello World from " << username;
 	return 0;
 }
